e91b3aea85417f7c2dddeb71e0b125e6b4bf968d
Philip <philip_angelin@hotmail.com>
2019-05-20 20:22:28 +0200

    Workshop dag 3 klar

--- relatedposts/relatedposts.php
+++ relatedposts/relatedposts.php
@@ -66,3 +66,12 @@ function relatedposts_init() {
     add_shortcode('related-posts', 'relatedposts_shortcode');
 }
 add_action('init', 'relatedposts_init');
+
+function relatedposts_the_content($content) {
+    if(is_single() && is_main_query() && in_the_loop() && !has_shortcode($content, 'related-posts')) {
+        $content .= get_related_posts();
+    }
+
+    return $content;
+}
+add_filter('the_content', 'relatedposts_the_content');
