a0b58bcaf06c837e0556c319775ceadf597787c5
Philip <philip_angelin@hotmail.com>
2019-05-21 20:50:03 +0200

    Lagt till settings sub-page för pluginet

--- customize_posts/settings.php
+++ customize_posts/settings.php
@@ -1,2 +1,25 @@
 <?php
 
+// Add settings page to the dashboard menu
+function cp_add_settings_page_to_menu() {
+    add_submenu_page(
+        'options-general.php', // Parent page
+        'Customize Posts', // Settings page title
+        'Customize Posts', // Menu title
+        'manage_options', // User capability requirement
+        'customize_posts', // Slug for settings page URL
+        'cp_settings_page' // Callback to render function
+    );
+}
+add_action('admin_menu', 'cp_add_settings_page_to_menu');
+
+// Add settings
+function cp_settings_init() {
+    //register_setting();
+}
+add_action('admin_init', 'cp_settings_init');
+
+// Render settings page
+function cp_settings_page() {
+    
+}
