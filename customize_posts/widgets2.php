9e49612fb3facd01079b7a27018d3b6ef777fa0a
Philip <philip_angelin@hotmail.com>
2019-05-23 05:35:01 +0200

    Har nu hämtat HTTP request och encodat datan

--- customize_posts/widgets2.php
+++ customize_posts/widgets2.php
@@ -1,9 +1,16 @@
 <?php
 
 require("class.latestpostswidget.php");
+require("class.starwarswidget.php");
 
-// Load Widgets
+// Load Latest Posts Widget
 function latestposts_widget_init() {
     register_widget('LatestPostsWidget');
 }
 add_action('widgets_init', 'latestposts_widget_init');
+
+// Load Star Wars Widget
+function starwars_widget_init() {
+    register_widget('StarWarsWidget');
+}
+add_action('widgets_init', 'starwars_widget_init');
